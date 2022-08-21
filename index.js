40e8d3149a0aa922ad2e59f5b1b0b376f013cf8e
H_A _Opu <wwwhasib4@gmail.com>
2022-08-21 06:14:46 +0600

    mongodb connection and create 2 get api

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jw3mngb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    // collections
+    const coursesCollection = client.db("courseia").collection("courses");
+
+    await client.connect();
+
+    app.get("/courses", async (req, res) => {
+      const courses = await coursesCollection.find({}).toArray();
+      res.send(courses);
+    });
+
+    app.get("/courses/:id", async (req, res) => {
+      const id = req.params.id;
+      const course = await coursesCollection.findOne({ _id: ObjectId(id) });
+      res.send(course);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send(`Courseia server is Running`);
+});
+
+app.listen(port, () => {
+  console.log(`Courseia app listening on PORT ${port}`);
+});
